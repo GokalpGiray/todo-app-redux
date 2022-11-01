6e014edbf1938dd07bd3782f964484e11f6ae5b0
Gokalp Giray <gokalpgiray24@gmail.com>
2022-11-01 17:20:24 +0300

    Genel görünüm hazırlandı.

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer class="info">
+            <p>Click to edit a todo</p>
+            <p>Created by <a href="https://gokalpgiray.github.io/Portfolio/">Gökalp Giray</a></p>
+        </footer>
+    )
+}
+
+export default Footer
